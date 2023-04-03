169b8c3b27b495141d894408d2e5e507c54d0adb
jatanael <jatanael.ods@gmail.com>
2023-04-03 12:27:44 -0300

    Primeiro commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import SideBar from "@/components/SideBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SideBar>
+      <Component {...pageProps} />
+    </SideBar>
+  );
 }
